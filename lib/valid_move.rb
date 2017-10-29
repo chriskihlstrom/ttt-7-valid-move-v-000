37d4e8324e280ae3ccb1aff6a4e013c42708aa8e
Chris Kihlstrom <ckihlst@gmail.com>
2017-10-29 18:26:46 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 def valid_move?(board,index)
   if def position_taken?(board,index)
     board[index] != " " && board[index] != "" && board[index] != nil
-  elsif board[index] !>= 0 && board[index] !<= 8
+  else board[index] !>= 0 && board[index] !<= 8
   end
 end
